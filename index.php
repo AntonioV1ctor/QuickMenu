62d4e3358dc1936d5a2944153b378771852bdd7c
Antônio Silva <antonio54098946@aluno.ms.senac.br>
2025-03-13 21:16:11 -0400

    Add project structure default 2

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php include_once "./view/components/head.php" ?>
+<body>
+    
+    <?php include_once "./view/components/footer.php" ?>
+</body>
+</html>
--- view/components/head.php
+++ view/components/head.php
@@ -1,9 +1,14 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
+    <script src="https://kit.fontawesome.com/db29c2ca45.js" crossorigin="anonymous"></script><!-- Icon -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/x-icon" href="view/assets/images/">
-    <link href="../assets/css/style.css">
+    <link rel="icon" type="image/x-icon" href="view/assets/images/favicon.ico">
+    <link rel="preconnect" href="https://fonts.googleapis.com"> 
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"><!-- Google Fonts -->
+    <link href="../assets/css/style.css"><!-- Css Main -->
     <title>QuickMenu</title>
 </head>
