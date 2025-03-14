086a13005e19fc46a38884c09e51bbd0f25e6c26
AntonioBaikal <antonio.silva@baikalsec.com>
2025-03-14 18:11:36 -0400

    Add docker-compose to start the project server and corrections in the css

--- index.php
+++ index.php
@@ -1,14 +1,22 @@
-<?php include_once "./view/components/head.php" ?>
+<?php include_once('./view/components/head.php'); ?>
+
 <body>
+    <?php include_once('./view/components/navbar.php');?>
     <div class="login-home-container">
+
         <div class="login-home-box">
-            <form action="" method="post">
-                <input required type="email">
-                <input required type="password">
-                <input required type="submit" value="sign in">
+            <form class="login-home-form" action="/view/pages/user/index.php" method="post">
+                <div class="login-home-align-input">
+                    <label for="flogin">Login</label>
+                    <input class="login-home-input-default" required type="email" name="flogin">
+                    <label for=" fpass">Password</label>
+                    <input class="login-home-input-default" required type="password" name="fpass">
+                </div>
+                <input class="login-home-button-default" required type="submit" value="sign in">
             </form>
         </div>
     </div>
-    <?php include_once "./view/components/footer.php" ?>
+    <?php include_once "./view/components/footer.php"; ?>
 </body>
+
 </html>
--- view/components/head.php
+++ view/components/head.php
@@ -6,9 +6,10 @@
     <script src="https://kit.fontawesome.com/db29c2ca45.js" crossorigin="anonymous"></script><!-- Icon -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="view/assets/images/favicon.ico">
-    <link rel="preconnect" href="https://fonts.googleapis.com"> 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"><!-- Google Fonts -->
-    <link href="../assets/css/style.css"><!-- Css Main -->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet"><!-- Google Fonts -->
+    <link rel="stylesheet" href="./view/assets/css/style.css"><!-- Css Main -->
     <title>QuickMenu</title>
 </head>
