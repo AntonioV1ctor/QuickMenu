594718bf3009ebd5784859ca2e68ec626c5dba29
Antônio Silva <antonio54098946@aluno.ms.senac.br>
2025-03-13 21:49:07 -0400

    Add simple modification

--- index.php
+++ index.php
@@ -2,7 +2,11 @@
 <body>
     <div class="login-home-container">
         <div class="login-home-box">
-            
+            <form action="" method="post">
+                <input required type="email">
+                <input required type="password">
+                <input required type="submit" value="sign in">
+            </form>
         </div>
     </div>
     <?php include_once "./view/components/footer.php" ?>
