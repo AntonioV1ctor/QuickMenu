ea6812faa7876099c507a1064f2e5152968b1e96
AntonioBaikal <antonio.silva@baikalsec.com>
2025-03-14 15:17:45 -0400

    Add docker-compose.yml and anyone files

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<?php include_once "./view/components/head.php" ?>
+<body>
+    <div class="login-home-container">
+        <div class="login-home-box">
+            <form action="" method="post">
+                <input required type="email">
+                <input required type="password">
+                <input required type="submit" value="sign in">
+            </form>
+        </div>
+    </div>
+    <?php include_once "./view/components/footer.php" ?>
+</body>
+</html>
