44a46b952c87465c349295d662d15d2d263d532e
Antônio Silva <antonio54098946@aluno.ms.senac.br>
2025-03-13 21:33:47 -0400

    RootCss,ResetCss and others modifications

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php include_once "./view/components/head.php" ?>
 <body>
-    
+    <div class="login-home-container">
+        <div class="login-home-box">
+            
+        </div>
+    </div>
     <?php include_once "./view/components/footer.php" ?>
 </body>
 </html>
