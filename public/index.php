083790407e5b67f55f69834f4561abf6140cc0a7
danilogames220 <daniloassuerolira@gmail.com>
2025-03-14 20:12:30 -0400

    added some files

--- public/index.php
+++ public/index.php
@@ -1,14 +1,15 @@
-<?php include_once "./view/components/head.php" ?>
+<?php include_once "../model/scripts/get_cookie.php"?>
+
+<?php include_once "../view/components/head.php" ?>
 <body>
-    <div class="login-home-container">
-        <div class="login-home-box">
-            <form action="" method="post">
-                <input required type="email">
-                <input required type="password">
-                <input required type="submit" value="sign in">
-            </form>
-        </div>
+    <h2 id="user-greeting">hello user</h2>
+    
+
+    <div id="menu">
+        menu
     </div>
-    <?php include_once "./view/components/footer.php" ?>
+
+    <?php include_once "../view/components/footer.php" ?>
 </body>
 </html>
+
---
+++ public/login/signin.php
@@ -0,0 +1,16 @@
+<?php include_once "../view/components/head.php" ?>
+<body>
+    <div class="login-home-container">
+        <div class="login-home-box">
+            <form action="" method="post">
+                <input required type="email">
+                <input required type="password">
+                <input required type="submit" value="sign in">
+            </form>
+        </div>i already have an account</button>
+    </div>
+    <?php include_once "../view/components/footer.php" ?>
+</body>
+</html>
+
+<?php include_once "../model/scripts/user.php"?>
